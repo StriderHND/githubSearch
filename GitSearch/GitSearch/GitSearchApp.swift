60666ac4d42a865de4380da051de1d00c7fd57a0
Erick Gonzales <neko.erick.mac@gmail.com>
2023-01-26 08:58:21 -0600

    chore: project init
    project initialization

---
+++ GitSearch/GitSearch/GitSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitSearchApp.swift
+//  GitSearch
+//
+//  Created by Erick Gonzales on 26/1/23.
+//
+
+import SwiftUI
+
+@main
+struct GitSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
